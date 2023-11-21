99736b01137eb479fa0e37be7e80700632174f6c
fikrialwan <fikriar93@gmail.com>
2023-11-21 22:00:37 +0700

    feat: adjust syntaks svg for react

--- src/component/icons/three-dots.tsx
+++ src/component/icons/three-dots.tsx
@@ -10,14 +10,14 @@ const ThreeDotsIcon = ({
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke={color || "white"}
       className={className || "w-6 h-6"}
       {...props}
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
       />
     </svg>
