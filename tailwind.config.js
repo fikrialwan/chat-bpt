58b282c61e05d9ded9b23e88dae409b845b3180c
fikrialwan <fikriar93@gmail.com>
2023-11-26 23:34:40 +0700

    fix: viewport mobile using webkit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,11 @@ export default {
         primaryBlue: "#29ABE2",
       },
       height: {
-        screen: ['100vh', '100dvh'],
+        screen: ["100vh", "100dvh", "-webkit-fill-available"],
       },
       maxHeight: {
-        screen: ['100vh', '100dvh'],
-      }
+        screen: ["100vh", "100dvh", "-webkit-fill-available"],
+      },
     },
   },
   plugins: [],
