73ceeb59b79f8510ab4b11988b8d3267c1b305bd
fikrialwan <fikriar93@gmail.com>
2023-12-10 15:51:02 +0700

    feat: update ui header

--- src/component/layout/header.tsx
+++ src/component/layout/header.tsx
@@ -3,7 +3,7 @@ import ThreeDotsIcon from "../icons/three-dots";
 
 const Header = () => {
   return (
-    <header className="py-3 px-4 bg-primaryBlue flex flex-row justify-between items-center shadow-md text-white">
+    <header className="py-3 px-4 bg-white flex flex-row justify-between items-center border-b border-gray-200 text-black">
       <div className="flex flex-row gap-4 justify-start items-center">
         <div className="w-12 h-12 relative">
           <img
@@ -13,11 +13,11 @@ const Header = () => {
           <span className="w-2 h-2 bg-green-500 rounded-full m-0 absolute bottom-[3px] right-[3px]" />
         </div>
         <div className="flex flex-col align-baseline justify-start">
-          <h1 className="text-xl font-normal">Bang Pitung</h1>
-          <p className="text-sm text-gray-100">Online</p>
+          <h1 className="text-xl font-semibold text-primaryBlue">Bang Pitung</h1>
+          <p className="text-sm font-light text-green-500">Online</p>
         </div>
       </div>
-      <ThreeDotsIcon className="w-8 h-8" />
+      <ThreeDotsIcon className="w-8 h-8" color="black" />
     </header>
   );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,7 @@ import Layout from "../component/layout";
 const Home = () => {
   return (
     <Layout>
-      <div className="bg-red-100 h-screen">hello</div>
+      <div className="h-screen">hello</div>
     </Layout>
   );
 };
