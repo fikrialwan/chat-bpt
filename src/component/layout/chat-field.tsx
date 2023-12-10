bebce3f6e36cb5e07f4e200d221501f16ae47f3c
fikrialwan <fikriar93@gmail.com>
2023-12-10 17:03:06 +0700

    feat: modify ui input message

--- src/component/layout/chat-field.tsx
+++ src/component/layout/chat-field.tsx
@@ -1,16 +1,17 @@
 import ReactTextareaAutosize from "react-textarea-autosize";
+import SendIcon from "../icons/send";
 
 const ChatField = () => {
   return (
     <div className="flex items-end w-full gap-4 px-6 py-3">
       <ReactTextareaAutosize
-        className="flex-1 resize-none py-3 px-6 rounded-xl bg-gray-100 outline-none"
+        className="flex-1 resize-none py-4 px-8 rounded-[32px] bg-white outline-none drop-shadow-2xl"
         minRows={1}
         maxRows={5}
         placeholder="Write something"
       />
-      <button className="py-3 px-6 bg-primaryBlue hover:bg-primaryBlue/90 text-white rounded-xl font-semibold">
-        Send
+      <button className="py-4 px-4 bg-primaryBlue hover:bg-primaryBlue/90 text-white rounded-full font-semibold">
+        <SendIcon color="white" />
       </button>
     </div>
   );
