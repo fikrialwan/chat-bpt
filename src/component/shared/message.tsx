f4e44e399ddc6c877f0610268999b4dbb52be4af
fikrialwan <fikriar93@gmail.com>
2023-12-17 13:59:26 +0700

    refactor: move message ui to shared component

---
+++ src/component/shared/message.tsx
@@ -0,0 +1,37 @@
+import profile from "../../assets/chat-bpt-profile.jpg";
+
+interface MessageProps {
+  children: string;
+}
+
+const Message = ({ children, isUser }: MessageProps & { isUser: boolean }) => {
+  return isUser ? (
+    <MessageUser>{children}</MessageUser>
+  ) : (
+    <MessageAI>{children}</MessageAI>
+  );
+};
+
+const MessageUser = ({ children }: MessageProps) => {
+  return (
+    <p className="px-6 py-3 rounded-3xl rounded-tr-none bg-primaryBlue text-white self-end">
+      {children}
+    </p>
+  );
+};
+
+const MessageAI = ({ children }: MessageProps) => {
+  return (
+    <div className="flex flex-row items-end gap-4">
+      <img
+        src={profile}
+        className="w-12 h-12 rounded-full border-2 outline-white"
+      />
+      <p className="px-6 py-3 rounded-3xl rounded-bl-none bg-primaryGray">
+        {children}
+      </p>
+    </div>
+  );
+};
+
+export default Message;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,14 @@
 import Layout from "../component/layout";
 import Container from "../component/shared/container";
-import profile from "../assets/chat-bpt-profile.jpg";
+import Message from "../component/shared/message";
 
 const Home = () => {
   return (
     <Layout>
       <Container>
         <div className="flex flex-col gap-4">
-          <p className="px-6 py-3 rounded-3xl rounded-tr-none bg-primaryBlue text-white self-end">
-            hi
-          </p>
-          <div className="flex flex-row items-end gap-4">
-            <img
-              src={profile}
-              className="w-12 h-12 rounded-full border-2 outline-white"
-            />
-            <p className="px-6 py-3 rounded-3xl rounded-bl-none bg-primaryGray">
-              hello
-            </p>
-          </div>
+          <Message isUser>hi bang pitung</Message>
+          <Message isUser={false}>hello, any questions ?</Message>
         </div>
       </Container>
     </Layout>
