7f10ce45b0cec092477ea1d59650450441a5e2a8
fikrialwan <fikriar93@gmail.com>
2023-12-17 13:15:37 +0700

    feat(chat-field): add container

--- src/component/layout/chat-field.tsx
+++ src/component/layout/chat-field.tsx
@@ -1,19 +1,22 @@
 import ReactTextareaAutosize from "react-textarea-autosize";
 import SendIcon from "../icons/send";
+import Container from "../shared/container";
 
 const ChatField = () => {
   return (
-    <div className="flex items-end w-full gap-4 px-6 py-3">
-      <ReactTextareaAutosize
-        className="flex-1 resize-none py-4 px-8 rounded-[32px] bg-white outline-none drop-shadow-2xl"
-        minRows={1}
-        maxRows={5}
-        placeholder="Write something"
-      />
-      <button className="py-4 px-4 bg-primaryBlue hover:bg-primaryBlue/90 text-white rounded-full font-semibold">
-        <SendIcon color="white" />
-      </button>
-    </div>
+    <Container>
+      <div className="flex items-end w-full gap-4 px-6 py-3">
+        <ReactTextareaAutosize
+          className="flex-1 resize-none py-4 px-8 rounded-[32px] bg-white outline-none drop-shadow-2xl"
+          minRows={1}
+          maxRows={5}
+          placeholder="Write something"
+        />
+        <button className="py-4 px-4 bg-primaryBlue hover:bg-primaryBlue/90 text-white rounded-full font-semibold">
+          <SendIcon color="white" />
+        </button>
+      </div>
+    </Container>
   );
 };
 
