68c66543faf8c45fb6304d659983ff060db997dc
fikrialwan <fikriar93@gmail.com>
2023-12-17 13:13:45 +0700

    feat(header): add container to header

--- src/component/layout/header.tsx
+++ src/component/layout/header.tsx
@@ -1,23 +1,30 @@
 import profile from "../../assets/chat-bpt-profile.jpg";
 import ThreeDotsIcon from "../icons/three-dots";
+import Container from "../shared/container";
 
 const Header = () => {
   return (
-    <header className="py-3 px-4 bg-white flex flex-row justify-between items-center border-b border-gray-200 text-black">
-      <div className="flex flex-row gap-4 justify-start items-center">
-        <div className="w-12 h-12 relative">
-          <img
-            src={profile}
-            className="w-12 h-12 rounded-full border-2 outline-white"
-          />
-          <span className="w-2 h-2 bg-green-500 rounded-full m-0 absolute bottom-[3px] right-[3px]" />
+    <header className="bg-white border-b border-gray-200 text-black">
+      <Container>
+        <div className="flex flex-row justify-between items-center">
+          <div className="flex flex-row gap-4 justify-start items-center">
+            <div className="w-12 h-12 relative">
+              <img
+                src={profile}
+                className="w-12 h-12 rounded-full border-2 outline-white"
+              />
+              <span className="w-2 h-2 bg-green-500 rounded-full m-0 absolute bottom-[3px] right-[3px]" />
+            </div>
+            <div className="flex flex-col align-baseline justify-start">
+              <h1 className="text-xl font-semibold text-primaryBlue">
+                Bang Pitung
+              </h1>
+              <p className="text-sm font-light text-green-500">Online</p>
+            </div>
+          </div>
+          <ThreeDotsIcon className="w-8 h-8" color="black" />
         </div>
-        <div className="flex flex-col align-baseline justify-start">
-          <h1 className="text-xl font-semibold text-primaryBlue">Bang Pitung</h1>
-          <p className="text-sm font-light text-green-500">Online</p>
-        </div>
-      </div>
-      <ThreeDotsIcon className="w-8 h-8" color="black" />
+      </Container>
     </header>
   );
 };
