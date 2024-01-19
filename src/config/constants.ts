922edb9a9b087e6922f9216cfc66cb09ca6f9dfe
fikrialwan <fikriar93@gmail.com>
2024-01-19 20:15:19 +0700

    feat: change login flag

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,4 +2,4 @@ export const BASE_URL = import.meta.env.VITE_BASE_URL || "";
 
 //feature flag
 export const LOGIN_FLAG: boolean =
-  import.meta.env.VITE_LOGIN_FLAG?.toLowerCase() === "true";
+  import.meta.env.VITE_LOGIN_FLAG?.toLowerCase() === "false";
